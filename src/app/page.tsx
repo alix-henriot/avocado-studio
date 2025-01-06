3f38d597ba3e51fb7189f0eab6e1fd423acda5f6
Alix Henriot <copycat.extension@gmail.com>
2025-01-06 20:42:15 +0100

    Add Carousel and CarouselItems with data array in the page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,39 @@ import { Button } from "@nextui-org/react";
 
 
 export default function Home() {
+  const carouselItems = [
+    {
+      title: 'Business Event',
+      description: 'We provide business event shooting.',
+      src: '/business-service.jpg',
+      alt: 'business photo',
+    },
+    {
+      title: 'Fashion',
+      description: 'We provide fashion shooting.',
+      src: '/fashion-service.jpg',
+      alt: 'fashion photo',
+    },
+    {
+      title: 'Concert',
+      description: 'We provide concert shooting.',
+      src: '/concert-service.jpg',
+      alt: 'concert photo',
+    },
+    {
+      title: 'Food Shooting',
+      description: 'We provide food shooting.',
+      src: '/food-service.jpg',
+      alt: 'food photo',
+    },
+    {
+      title: 'Product Shooting',
+      description: 'We provide product shooting.',
+      src: '/product-service.jpg',
+      alt: 'product photo',
+    },
+  ];
+
   return (
     <div
     className="min-h-screen flex flex-col"
@@ -21,7 +54,7 @@ export default function Home() {
           <Button color='default' size='lg' className="w-fit" radius='full'>Request a quote</Button>
         </FeatureCard>
 
-        <Carousel/>
+        <Carousel items={carouselItems}/>
 
         <FeatureCard title='Audiovisual Studio' subtitle='Expert in Content' src='/feature-image-1.jpg' alt='Photographer editing photos'/>
 
