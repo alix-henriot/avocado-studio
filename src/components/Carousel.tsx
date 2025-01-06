6cec9b605163427598e7865074f2845842333e87
Alix Henriot <copycat.extension@gmail.com>
2025-01-06 21:02:42 +0100

    Add Carousel title and subtitle Refine padding, font weight and tracking

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,17 +4,26 @@ import React from 'react'
 import { motion } from 'framer-motion';
 
 type CarouselProps = {
-    items: CarouselItemProps[]
+    title: string;
+    subtitle: string;
+    items: CarouselItemProps[];
 }
 
-const Carousel: React.FC<CarouselProps> = ({items}) => {
+const Carousel: React.FC<CarouselProps> = ({title, subtitle, items}) => {
   return (
     <div
-    className='flex flex-row items-end gap-5 p-5 my-8 h-[620px] overflow-x-scroll'
+    className='flex flex-col my-16 text-center'
     >
-        {items.map((item, index) => (
-            <CarouselItem key={index} title={item.title} description={item.description} src={item.src} alt={item.alt}/>
-        ))}
+        <h3 className='font-semibold text-5xl tracking-tight mb-3'>{title}</h3>
+        <p className='font-medium text-2xl'>{subtitle}</p>
+
+        <div
+        className='flex flex-row items-end px-5 gap-5 h-[520px] overflow-x-scroll'
+        >
+            {items.map((item, index) => (
+                <CarouselItem key={index} title={item.title} description={item.description} src={item.src} alt={item.alt}/>
+            ))}
+        </div>
     </div>
   )
 }
@@ -35,10 +44,10 @@ const CarouselItem: React.FC<CarouselItemProps> = ({title, description, src, alt
         className='relative flex flex-col p-6 w-[400px] h-[400px] bg-gray-200 rounded-3xl flex-shrink-0 overflow-clip scrollbar-hide'
         >
             <div
-            className='flex flex-col gap-2 z-10'
+            className='flex flex-col items-start gap-2 z-10'
             >
-                <span className='font-medium text-4xl text-white'>{title}</span>
-                <p className='text-lg text-white'>{description}</p>
+                <span className='font-semibold text-4xl tracking-tight text-white'>{title}</span>
+                <p className='font-medium text-lg text-white'>{description}</p>
             </div>
             <Image
                 className='absolute -z-0 object-fill'
