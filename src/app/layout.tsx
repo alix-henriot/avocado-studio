4d67342bc33ea5c9dfca75b9d4c9cdb38a3b4cc5
Alix Henriot <henriotal@icloud.com>
2025-01-07 19:14:57 +0100

    Add and configure tailwindcss-debug-screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${interSans.variable} antialiased`}
+        className={`${interSans.variable} antialiased debug-screens`}
       >
         <NextUIProvider>
           {children}
