e44bebe859bf5a4fa6bed31591e727c3f46b885e
Alix Henriot <henriotal@icloud.com>
2025-01-07 19:47:30 +0100

    Switch to Roboto_Flex font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Flex } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 
-const interSans = Inter({
-  variable: "--font-inter-sans",
+const robotoFlex = Roboto_Flex({
+  variable: "--font-roboto-sans",
   subsets: ["latin"],
 });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${interSans.variable} antialiased debug-screens`}
+        className={`${robotoFlex.variable} antialiased debug-screens`}
       >
         <NextUIProvider>
           {children}
