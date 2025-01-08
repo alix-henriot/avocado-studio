79a62258801300c3879ef451e8d9f283ccbd214b
Alix Henriot <henriotal@icloud.com>
2025-01-08 18:01:21 +0100

    Edit text width of SectionTitle component and add it in HeroCard component

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -11,12 +11,12 @@ const SectionTitle: React.FC<SectionTitleProps> = ({title, subtitle}) => {
     className='relative flex flex-col text-center z-10'
     >
         <h2
-        className='mb-2 font-normal text-2xl tracking-tight'
+        className='mb-2 font-medium text-2xl tracking-tight'
         >
             {title}
         </h2>
         <h3
-        className='font-bold text-5xl tracking-tight mb-3'
+        className='font-semibold text-5xl tracking-tight mb-3'
         >
             {subtitle}
         </h3>
