2276cd5d93d84f2798d149c033e1612e5e23075e
Alix Henriot <henriotal@icloud.com>
2025-01-08 20:06:28 +0100

    Replace scale change on scroll with y translation

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -20,13 +20,14 @@ const FeatureCard: React.FC<FeatureCardProps> = ({title, subtitle, src, alt, chi
         offset: ['start end', 'center center'],
     });
 
-    const scale = useTransform(scrollYProgress, [0, 1], [0.85, 1]);
-    const opacity = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+    //const scale = useTransform(scrollYProgress, [0, 1], [0.85, 1]);
+    const y = useTransform(scrollYProgress, [0, 1], [100, 0]);
+    const opacity = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
 
   return (
     <motion.div
     ref={ref}
-    style={{ scale, opacity }}
+    style={{ y, opacity }}
     className='relative container flex flex-col gap-5 p-20 mx-auto my-8 w-full h-[620px] rounded-3xl overflow-clip'
     >
         <div
--- src/components/FeatureList.tsx
+++ src/components/FeatureList.tsx
@@ -22,13 +22,13 @@ const FeatureList: React.FC<FeatureListProps> = ({title, subtitle, items}) => {
         offset: ['start end', 'center center'],
     });
 
-    const scale = useTransform(scrollYProgress, [0, 1], [0.85, 1]);
-    const opacity = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+    const y = useTransform(scrollYProgress, [0, 1], [100, 0]);
+    const opacity = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
 
   return (
     <motion.div
     ref={ref}
-    style={{ scale, opacity }}
+    style={{ y, opacity }}
     className='relative container flex flex-col gap-16 p-20 mx-auto my-8 w-full h-[620px] bg-default rounded-3xl overflow-clip'
     >
         <SectionTitle title={title} subtitle={subtitle}/>
