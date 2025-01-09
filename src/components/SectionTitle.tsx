c59ba0ffcd922eacdcd64cf47a2a0764049301dc
Alix Henriot <henriotal@icloud.com>
2025-01-09 23:13:15 +0100

    Add className props to SectionTitle, improve responsiveness

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,22 +1,24 @@
+import { cn } from '@/lib/utils';
 import React from 'react'
 
 type SectionTitleProps = {
+    className?: string;
     title: string;
     subtitle: string;
 }
 
-const SectionTitle: React.FC<SectionTitleProps> = ({title, subtitle}) => {
+const SectionTitle: React.FC<SectionTitleProps> = ({className, title, subtitle}) => {
   return (
     <div
-    className='relative flex flex-col text-center z-10'
+    className={cn('relative z-10 mx-auto', className)}
     >
         <h2
-        className='mb-2 font-medium text-2xl tracking-tight'
+        className='font-medium lg:mb-1 text-lg lg:text-2xl tracking-tight'
         >
             {title}
         </h2>
         <h3
-        className='font-semibold text-5xl tracking-tight mb-3'
+        className='font-semibold text-3xl lg:text-5xl tracking-tight mb-3'
         >
             {subtitle}
         </h3>
