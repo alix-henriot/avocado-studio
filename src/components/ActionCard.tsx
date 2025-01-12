1ab35734e9eb6315948107a7d1cdbdceed3fc374
Alix Henriot <copycat.extension@gmail.com>
2025-01-12 19:09:19 +0100

    Remove Framer Motion scroll effect

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -1,9 +1,6 @@
 'use client'
 import React, { useRef } from 'react'
-import { motion, useInView, useScroll, useTransform } from 'framer-motion';
-import Image from 'next/image';
 import SectionTitle from './SectionTitle';
-import { Button } from '@nextui-org/react';
 
 type ActionCardProps = {
     title: string;
@@ -14,29 +11,19 @@ type ActionCardProps = {
 }
 
 const ActionCard: React.FC<ActionCardProps> = ({title, subtitle, src, alt, children}) => {
-    const ref = useRef<HTMLDivElement>(null);
-    const isInView = useInView(ref, { once: false });
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ['start end', 'center center'],
-    });
-
-    const y = useTransform(scrollYProgress, [0, 1], [64, 0]);
-    const opacity = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-
   return (
-    <section className='px-3 lg:px-0'>
-    <motion.div
-    ref={ref}
-    style={{ y, opacity }}
-    className='relative container grid gap-3 p-6 lg:p-20 mx-auto my-16 max-w-screen md:max-w-5xl  md:aspect-[4/1] rounded-2xl lg:rounded-3xl overflow-clip'
+    <section className='px-3 lg:px-0 snap-start'>
+    <div
+    className='relative container grid sm:grid-cols-4 gap-3 p-6 lg:p-10 mx-auto my-16 items-center max-w-screen md:max-w-5xl sm:aspect-[4/1] rounded-2xl lg:rounded-3xl overflow-clip'
     >
-
-        <SectionTitle className='text-center text-white' title={title} subtitle={subtitle}/>
+        <SectionTitle className='text-center sm:text-left sm:col-span-3 text-white' title={title} subtitle={subtitle}/>
         <div className='z-10 mx-auto'>
             {children}
         </div>
-        <Image
+        <div
+            className='absolute z-[1] bg-gradient-to-t from-green-800 to-default w-full h-full'
+            />
+        {/* <Image
         className='absolute -z-0 object-fill'
         src={src}
         alt={alt}
@@ -44,8 +31,8 @@ const ActionCard: React.FC<ActionCardProps> = ({title, subtitle, src, alt, child
         fill
         objectFit='cover'
         loading='lazy'
-        />
-    </motion.div>
+        /> */}
+    </div>
     </section>
   )
 }
