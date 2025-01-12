e7fb10a8c6a2d0c33139b9902da988491ab59abb
Alix Henriot <copycat.extension@gmail.com>
2025-01-12 19:12:04 +0100

    Remove Framer Motion scroll effect Improve title and subtitle display, add props such as children and className for individual personalization

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image';
 import React, { useRef } from 'react'
-import { motion, useInView, useScroll, useTransform } from 'framer-motion';
+//import { motion, useInView, useScroll, useTransform } from 'framer-motion';
 import SectionTitle from './SectionTitle';
 import { cn } from '@/lib/utils';
 
@@ -15,22 +15,20 @@ type FeatureCardProps = {
 }
 
 const FeatureCard: React.FC<FeatureCardProps> = ({className, title, subtitle, src, alt, children}) => {
-    const ref = useRef<HTMLDivElement>(null);
+    /* const ref = useRef<HTMLDivElement>(null);
     const isInView = useInView(ref, { once: false });
     const { scrollYProgress } = useScroll({
         target: ref,
         offset: ['start end', 'center center'],
     });
 
-    //const scale = useTransform(scrollYProgress, [0, 1], [0.85, 1]);
+    const scale = useTransform(scrollYProgress, [0, 1], [0.85, 1]);
     const y = useTransform(scrollYProgress, [0, 1], [24, 0]);
-    const opacity = useTransform(scrollYProgress, [0, 1], [0.2, 1]);
+    const opacity = useTransform(scrollYProgress, [0, 1], [0.2, 1]); */
 
   return (
     <section className='px-3 lg:px-0 snap-center snap-always'>
-      <motion.div
-      //ref={ref}
-      //style={{ y, opacity }}
+      <div
       className='relative container grid grid-cols-1 place-items-end p-6 lg:p-20 my-3 sm:my-5 mx-auto max-w-screen md:max-w-5xl aspect-[3/4] sm:aspect-video rounded-2xl lg:rounded-3xl overflow-clip'
       >
           <span className='mx-auto z-10'>{children}</span>
@@ -49,7 +47,7 @@ const FeatureCard: React.FC<FeatureCardProps> = ({className, title, subtitle, sr
           loading='lazy'
           />
         )}
-      </motion.div>
+      </div>
     </section>
   )
 }
