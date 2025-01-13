f7202a68231977c5d4c94328c7d041f3c6df6830
Alix Henriot <copycat.extension@gmail.com>
2025-01-13 22:30:40 +0100

    Wrap Button components in Link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,7 @@ import { motion, useInView, useScroll, useTransform } from 'framer-motion';
 import SectionTitle from './SectionTitle';
 import { Button } from '@nextui-org/react';
 import BackgroundMarquee from './BackgroundMarquee';
+import Link from 'next/link';
 
 type HeroProps = {
     title: string;
@@ -22,7 +23,9 @@ const Hero: React.FC<HeroProps> = ({title, subtitle, src, alt, children}) => {
         >
 
             <SectionTitle className='text-center text-foreground max-w-72 md:max-w-full mb-4' title={title} subtitle={subtitle}/>
-            <Button variant='shadow' color='success' size='md' className='w-fit mb-8 z-10 mx-auto' radius='full'>Instant quote</Button>
+            <Link href="/quote" passHref>
+              <Button as='a' variant='shadow' color='success' size='md' className='w-fit mb-8 z-10 mx-auto' radius='full'>Instant quote</Button>
+            </Link>
             <BackgroundMarquee/>
             <div className="pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-background"></div>
             <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-background"></div>
