9a302e15a419ec3a871bbd7b28e46a18826a0d5e
Alix Henriot <copycat.extension@gmail.com>
2025-01-21 20:52:12 +0100

    Add className prop to FormProgress.tsx

--- src/components/form/FormProgress.tsx
+++ src/components/form/FormProgress.tsx
@@ -1,15 +1,17 @@
 import { Progress } from '@nextui-org/react';
 import React from 'react'
+import { cn } from '@/lib/utils'
 
 type FormProgressProps = {
+    className?: string;
     minValue?: number;
     maxValue: number;
     currentStep: number;
 }
 
-const FormProgress: React.FC<FormProgressProps> = ({minValue = 0, maxValue, currentStep}) => {
+const FormProgress: React.FC<FormProgressProps> = ({minValue = 0, maxValue, currentStep, className}) => {
   return (
-    <nav className='p-4 max-w-sm mx-auto'>
+    <nav className={cn('p-4 max-w-sm mx-auto', className)}>
           <Progress
             minValue={minValue}
             maxValue={maxValue - 1}
