8f157985397ac7c43de9be1aa181b7454612bf48
Alix Henriot <copycat.extension@gmail.com>
2025-01-26 23:13:57 +0100

    Recreate /quote/page.tsx with better use of react-hook-form yet with no React Suspense and lazy load. Form sections now appear when the last field of the previous section has a value. Remove unecessary Controller. Remove useState for form step. Basic motion appear animation

--- src/app/quote/page.tsx
+++ src/app/quote/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import {
   Autocomplete,
   AutocompleteItem,
@@ -6,138 +6,275 @@ import {
   Calendar,
   Checkbox,
   CheckboxGroup,
+  DatePicker,
   Input,
   Select,
   SelectItem,
+  Switch,
   TimeInput,
 } from "@nextui-org/react";
-import { v4 as uuidv4 } from "uuid"; 
-import { useForm, FormProvider } from "react-hook-form";
-import { useAsyncList } from "@react-stately/data";
-import FormProgress from "@/components/form/FormProgress";
-import { LoaderCircle } from "lucide-react";
-import { debounce } from "lodash";
-import FormStartChat from "@/components/form/FormStartChat";
+import {
+  useForm,
+  FormProvider,
+  SubmitHandler,
+  Controller,
+} from "react-hook-form";
 import React, { Suspense } from "react";
-import { quoteGenerator } from "@/components/server/quoteGenerator";
-import saveForm from "@/components/server/saveForm";
+import { motion } from "framer-motion";
+import { useAsyncList } from "@react-stately/data";
+import Nav from "@/components/ui/Nav";
+import { getPriceQuote } from "@/components/server/pricing";
 
-export type FormData = {
-  forEach(arg0: (value: any, key: any) => void): unknown;
-  id?: string;
-  material: "photos" | "videos" | "photos-videos";
-  service: "fashion" | "business" | "food" | "product";
+export type FormValues = {
+  material: Array<'photos' | 'videos'>;
+  service: 'fashion' | 'event' | 'food' | 'product' | 'wedding';
+  editing: boolean;
+  unit: number;
   name: string;
   company: string;
   email: string;
   city: string;
-  setting: ("indoor" | "outdoor" | "studio")[];
-  date: { day: number; month: number; year: number };
-  from: { hour: number; minute: number };
-  to: { hour: number; minute: number };
-  options: ("editing" | "location-search" | "hotel-booking")[];
+  setting: Array<'indoor' | 'outdoor' | 'studio'>;
+  date: any;
+  coordinates: [number, number]; 
 };
 
-const steps = [
-  {
-    id: 0,
-    name: "service",
-    component: React.lazy(() => import("@/components/form/steps/ServiceStep")),
-  },
-  {
-    id: 1,
-    name: "contact",
-    component: React.lazy(() => import("@/components/form/steps/ContactStep")),
-  },
-  {
-    id: 2,
-    name: "city",
-    component: React.lazy(() => import("@/components/form/steps/PlaceStep")),
-  },
-  {
-    id: 3,
-    name: "date",
-    component: React.lazy(() => import("@/components/form/steps/DateStep")),
-  },
-  {
-    id: 4,
-    name: "options",
-    component: React.lazy(() => import("@/components/form/steps/OptionStep")),
-  },
-];
+type ServiceUnit = {
+  fashion: "set";
+  event: "event";
+  product: "set";
+  food: "set";
+};
+
+const serviceUnits: ServiceUnit = {
+  fashion: "set",
+  event: "event",
+  product: "set",
+  food: "set",
+};
 
 function Form() {
   const [currentStep, setCurrentStep] = React.useState(0);
-  const { control, handleSubmit, setValue, trigger, watch } = useForm<FormData>({
-    mode: 'onChange',
-    shouldUseNativeValidation: true,
-    shouldUnregister: false,
-    //defaultValues: {}
-  })
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setValue,
+  } = useForm<FormValues>({
+    mode: "onChange",
+    shouldUnregister: true,
+  });
 
-  const handleNext = async () => {
-    const isStepValid = await trigger();
-    console.log('isStepValid',isStepValid)
-    if (isStepValid) setCurrentStep((prevStep: number) => prevStep + 1);
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    console.log(data);
+    const quote = await getPriceQuote(data as FormValues)
+    console.log(quote)
+  }
+
+  const shootingTypeValue = watch("service");
+  const materialValue = watch("material");
+  const settingValue = watch("setting");
+
+  type FrenchCitiesProps = {
+    nom: string;
+    code: string;
+    _score: number;
+    centre: { type: string, coordinates: [number, number]}
+    departement: { code: number; nom: string };
   };
+  
 
-  const handlePrevious = () => {
-    if (currentStep > 0) {
-      setCurrentStep(currentStep - 1);
-    }
-  };
+    let list = useAsyncList<FrenchCitiesProps>({
+      async load({ signal, filterText }) {
+        let res = await fetch(
+          `https://geo.api.gouv.fr/communes?nom=${filterText}&boost=population&fields=code,nom,centre`,
+          { signal }
+        );
+        let json = await res.json();
 
-  const onSubmit = async (formData: FormData) => {
-    //submit data, get an id and push to /quote/id
-    const result = await saveForm(formData);
+        //console.log(JSON.stringify(json))
+  
+        return {
+          items: json,
+        };
+      },
+    });
 
-    if (result?.redirectUrl) {
-      window.location.href = result.redirectUrl; // Perform the redirect manually
-    }
-  };
-
-  const renderStep = React.useCallback(
-    (stepIndex: number) => {
-      const StepComponent = steps[stepIndex].component;
-      return (
-        <div key={steps[stepIndex].id}>
-          <StepComponent
-            control={control}
-            setValue={setValue}
-          />
-        </div>
-      );
-    },
-    [steps]
-  );
+    const unit = shootingTypeValue ? serviceUnits[shootingTypeValue] : "";
 
   return (
     <main className="min-h-screen">
-      <div className="container p-8 mx-auto md:max-w-xl">
-        <FormProgress currentStep={currentStep} maxValue={steps.length} />
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Suspense
-              fallback={
-                <div>
-                  <LoaderCircle className="animate-spin mx-auto" />
-                </div>
-              }
+      <Nav/>
+      <div className="container p-8 mx-auto md:max-w-2xl">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="grid grid-flow-row gap-4 mb-6"
+          >
+            <Select
+              placeholder="Fashion, Business event..."
+              label="Shooting type"
+              isRequired
+              {...register("service")}
             >
-              {renderStep(currentStep)}
-            </Suspense>
-            <div className="flex mt-4 w-full justify-between">
-              {currentStep > 0 && (
-                <Button onPress={handlePrevious}>Previous</Button>
+              <SelectItem value="fashion" key="fashion">
+                Fashion
+              </SelectItem>
+              <SelectItem value="event" key="event">
+                Event
+              </SelectItem>
+              <SelectItem value="product" key="product">
+                Product
+              </SelectItem>
+              <SelectItem value="food" key="food">
+                Food
+              </SelectItem>
+            </Select>
+
+            <Controller
+              name="material"
+              control={control}
+              render={({ field }) => (
+                <CheckboxGroup
+                  isRequired
+                  label="What kind of material"
+                  orientation="horizontal"
+                  aria-label="Material"
+                  {...field}
+                >
+                  <Checkbox value="photos">Photos</Checkbox>
+                  <Checkbox value="videos">Videos</Checkbox>
+                </CheckboxGroup>
               )}
-              {currentStep < steps.length - 1 && (
-                <Button type='button' onPress={handleNext}>Next</Button>
-              )}
-              {currentStep === steps.length - 1 && (
-                <Button type='submit'>Get Quote</Button>
-              )}
-            </div>
-          </form>
-          <FormStartChat step={currentStep}/>
+            />
+          </motion.div>
+
+            {shootingTypeValue && materialValue?.length > 0 && (
+              <motion.div
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                className="grid grid-flow-row gap-4 mb-6"
+              >
+                <Switch
+                  {...register("editing")}
+                  isDisabled={
+                    materialValue?.length === 1 && materialValue[0] === "photos"
+                  }
+                  defaultSelected={
+                    materialValue?.length === 1 && materialValue[0] === "photos"
+                  }
+                >
+                  {materialValue?.length === 1 && materialValue[0] === "photos"
+                    ? "Editing is included"
+                    : "Do you need editing?"}
+                </Switch>
+
+                <Input
+                  label={`Enter the number of ${unit}`}
+                  isRequired
+                  defaultValue="1"
+                  labelPlacement="outside"
+                  description={`Specify the quantity of ${unit}(s) required for this shoot.`}
+                  endContent={
+                    <div className="pointer-events-none flex items-center">
+                      <span className="text-default-400 text-small">{unit}</span>
+                    </div>
+                  }
+                  type="number"
+                  {...register("unit")}
+                />
+
+              <Controller
+                name="city"
+                control={control}
+                render={() => (
+                  <Autocomplete
+                    label="City"
+                    placeholder="Please choose a city"
+                    isRequired
+                    inputValue={list.filterText}
+                    isLoading={list.isLoading}
+                    items={list.items}
+                    onInputChange={list.setFilterText}
+                    {...register("city")}
+                    onSelectionChange={(key: any | null) => {
+                      if (key) {
+                        // Find the selected city based on the key (which is the code)
+                        const selectedCity = list.items.find((item) => item.code === key);
+                        if (selectedCity) {
+                          //setValue("city", selectedCity.nom);
+                          setValue("coordinates", selectedCity.centre.coordinates);
+                        }
+                      }
+                    }}
+                  >
+                    {(item: FrenchCitiesProps) => (
+                      <AutocompleteItem key={item.code} value={item.nom} className="capitalize">
+                        {item.nom}
+                      </AutocompleteItem>
+                    )}
+                  </Autocomplete>
+                )}
+              />
+                <Controller
+                  name="date"
+                  control={control}
+                  render={({ field }) => (
+                    <DatePicker label='Date' isRequired {...field} />
+                  )}
+                />
+                <Controller
+                  name="setting"
+                  control={control}
+                  render={({ field }) => (
+                    <CheckboxGroup
+                      isRequired
+                      label="Setting"
+                      aria-label="Setting"
+                      {...field}
+                      orientation="horizontal"
+                      //onChange={(setting) => setValue('setting', setting)}
+                      //onChange={(value) => control.register("setting")}
+                    >
+                      <Checkbox value="outdoor">Outdoor</Checkbox>
+                      <Checkbox value="indoor">Indoor</Checkbox>
+                      <Checkbox value="studio">Studio</Checkbox>
+                    </CheckboxGroup>
+                  )}
+                />
+                </motion.div>
+
+            )} 
+
+            {settingValue?.length > 0 && (
+
+              <motion.div
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              className="grid grid-flow-row gap-4 mb-6"
+              >
+                <Input
+                  label="Email"
+                  type="email"
+                  {...register("email")}
+                  isRequired
+                  />
+                <Input label="Name" {...register("name")} isRequired />
+                <Input label="Company" {...register("company")} isRequired />
+            </motion.div>
+            )}
+
+
+          <Button type="submit" color="primary" className="w-full mt-8">
+            Get a quote
+          </Button>
+        </form>
       </div>
     </main>
   );
