226fc54634534a8d7afd1399c0160cb556c1e30f
Alix Henriot <copycat.extension@gmail.com>
2025-01-26 23:14:27 +0100

    Extend allowed domains to fetch image urls

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com', 'picsum.photos', 'app.requestly.io']
+    domains: ['images.unsplash.com', 'source.unsplash.com', 'api.unsplash.com', 'picsum.photos', 'app.requestly.io']
   }
 };
 
